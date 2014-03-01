7fbcd1fc55da701af10b5c1881feef396999a6d3
Omer Wazir <omer.wazir@gmail.com>
2014-03-01 16:45:38 -0700

    Added basic login method to login controller

--- public/app/account/candyNavBarLoginCtrl.js
+++ public/app/account/candyNavBarLoginCtrl.js
@@ -1,5 +1,11 @@
-angular.module('candyApp').controller('candyNavBarLoginCtrl', function($scope){
+angular.module('candyApp').controller('candyNavBarLoginCtrl', function($scope,$http){
     $scope.signin = function(username,password){
-        console.log('Login code not done')
+        $http.post('/login', {username:username, password:password}).then(function(response){
+            if(response.data.success){
+                console.log('logged in!');
+            } else {
+                console.log('failed to log in!');
+            }
+        })
     };
 });
