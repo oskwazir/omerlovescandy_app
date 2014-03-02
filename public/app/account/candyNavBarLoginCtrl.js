ea9021ab2ecd64c8f8b725195f335e2a79b91cc5
omer.wazir <omer.wazir@gmail.com>
2014-03-01 18:23:10 -0700

    Added identity and notifier services to controller

--- public/app/account/candyNavBarLoginCtrl.js
+++ public/app/account/candyNavBarLoginCtrl.js
@@ -1,11 +1,13 @@
-angular.module('candyApp').controller('candyNavBarLoginCtrl', function($scope,$http){
+angular.module('candyApp').controller('candyNavBarLoginCtrl', function($scope,$http,candyIdentity,candyNotifier){
+	$scope.identity = candyIdentity;
     $scope.signin = function(username,password){
         $http.post('/login', {username:username, password:password}).then(function(response){
             if(response.data.success){
-                console.log('logged in!');
+            	candyIdentity.currentUser = response.data.user;
+               	candyNotifier.success('You have successfully signed in!');
             } else {
-                console.log('failed to log in!');
-            }
-        })
+                candyNotifier.error('Username/Password combination is incorrect');
+        	}
+    	})
     };
 });
