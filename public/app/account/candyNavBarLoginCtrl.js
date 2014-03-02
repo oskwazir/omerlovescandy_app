531ebe30f7cb88c747949310fa4b1532cb7c5b25
omer.wazir <omer.wazir@gmail.com>
2014-03-01 18:38:48 -0700

    Took auth code out

--- public/app/account/candyNavBarLoginCtrl.js
+++ public/app/account/candyNavBarLoginCtrl.js
@@ -1,13 +1,13 @@
-angular.module('candyApp').controller('candyNavBarLoginCtrl', function($scope,$http,candyIdentity,candyNotifier){
+angular.module('candyApp').controller('candyNavBarLoginCtrl',
+ function($scope, $http, candyIdentity, candyNotifier, candyAuth){
 	$scope.identity = candyIdentity;
     $scope.signin = function(username,password){
-        $http.post('/login', {username:username, password:password}).then(function(response){
-            if(response.data.success){
-            	candyIdentity.currentUser = response.data.user;
-               	candyNotifier.success('You have successfully signed in!');
-            } else {
-                candyNotifier.error('Username/Password combination is incorrect');
+    	candyAuth.authenticateUser(username,password).then(function(success){
+    		if(success){
+				candyNotifier.success('You have successfully signed in!');
+    		} else {
+    			candyNotifier.error('Username/Password combination is incorrect');
         	}
-    	})
+    	});
     };
 });
