f42689aeddd00ca0568e25f182be6c5c4bdebc08
omer.wazir <omer.wazir@gmail.com>
2014-03-01 18:21:33 -0700

    Created notifier service that uses Toastr

---
+++ public/app/common/candyNotifier.js
@@ -0,0 +1,14 @@
+angular.module('candyApp').value('candyToastr', toastr);
+
+angular.module('candyApp').factory('candyNotifier', function(candyToastr){
+	return {
+		success: function(msg){
+			candyToastr.success(msg);
+			console.log(msg);
+		},
+		error: function(msg){
+			candyToastr.error(msg);
+			console.log(msg);
+		}
+	}	
+});
